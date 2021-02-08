3223e26bb5a2731601ddd80e2b04fb787596c179
Shin, Seong-il <callmeshin75@gmail.com>
2021-02-09 02:33:51 +0900

    study for nestjs

---
+++ seongil/nest.js/hi-nest/src/movies/movies.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateMovieDto } from 'src/dto/create-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesServiece: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesServiece.getAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: number) {
+    return this.moviesServiece.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.moviesServiece.create(movieData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: number) {
+    return this.moviesServiece.deleteOne(movieId);
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: number, @Body() updateData) {
+    return this.moviesServiece.update(movieId, updateData);
+  }
+}
